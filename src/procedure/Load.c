d9745cad480d7c51f63a0ca3aeba8bc278a45411
Dhafin <141107867+dhafin00@users.noreply.github.com>
2024-11-25 17:09:05 +0700

    Update Load.c

--- src/procedure/Load.c
+++ src/procedure/Load.c
@@ -2,16 +2,35 @@
 #include <stdlib.h>
 
 
-void Load(){
-  int fileAda = 0;
+void Load(const char* filename){
+  FILE *file;
+  char path[100];  
+  int i = 0;
+  int j = 0;
+  const char *folder = "/save/";
 
-  if (fileAda = 1) {
-    printf("Save file berhasil dibaca. PURRMART berhasil dijalankan.")
+  
+  while (path[i] != '\0') {
+    path[j++] = folder[i++];
+  }
+  i = 0;
+  while (filename[i] != '\0') {
+    path[j++] = filename[i++];
+  }
+  path[j] = '0';
+
+ 
+  file = fopen(path, "r");
+  if (file) {
+    printf("Save file berhasil dibaca. PURRMART berhasil dijalankan.");
+    fclose(file);
   } else {
-    printf("Save file tidak ditemukan. PURRMART gagal dijalankan.")
+    printf("Save file tidak ditemukan. PURRMART gagal dijalankan.");
   }  
-
   return;
 }
 
+int main () {
+  return 0;
+}
 
