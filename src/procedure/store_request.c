380db359e353fa2c1092e84b19555b7642445fa6
SArdyBramantyo <168545215+SArdyBramantyo@users.noreply.github.com>
2024-11-25 20:11:56 +0700

    Update store_request.c

--- src/procedure/store_request.c
+++ src/procedure/store_request.c
@@ -15,62 +15,50 @@ void StoreRequest(ArrayDin arr, Queue* requestQueue){
     
     startKataMajemuk(NULL);
     /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
-    while (!is_same_string(currentKata.buffer, "Selesai")){
-        boolean Mark_Antrean;
-        printf("%d", isEmpty(*requestQueue));
-        if (isEmpty(*requestQueue)){
-            enqueue(requestQueue, currentKata.buffer);
-        }
+    boolean Mark_Antrean;
+    if (isEmpty(*requestQueue)){
+        enqueue(requestQueue, currentKata.buffer);
+    }
 
-        /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
+    /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
+    
+    else{
+        Mark_Antrean = contains(*requestQueue, currentKata.buffer);
+    }
         
-        else{
-            Mark_Antrean = contains(*requestQueue, currentKata.buffer);
-        }
-            
-        /*Mengecek apakah nama tersebut ada pada queue*/
+    /*Mengecek apakah nama tersebut ada pada queue*/
 
-        /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
-        int Mark_Supply = 0;
-        int N = getArrayDinLength(arr);
+    /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
+    int Mark_Supply = 0;
+    int N = getArrayDinLength(arr);
 
-        if (N > 0){
-                for (int j = 0; j < N; j++){
-                    Barang storelist = getArrayDinElmt(arr,j);
-                    
-                    if (is_same_string(currentKata.buffer, storelist.name)){
-                        Mark_Supply++;
-                    }
+    if (N > 0){
+            for (int j = 0; j < N; j++){
+                Barang storelist = getArrayDinElmt(arr,j);
+                
+                if (is_same_string(currentKata.buffer, storelist.name)){
+                    Mark_Supply++;
                 }
-            /*Meskipun hanya memakai item_name, tetap perlu diambil harga_item untuk memisahkannya*/  
-        }
-        
+            }
+        /*Meskipun hanya memakai item_name, tetap perlu diambil harga_item untuk memisahkannya*/  
+    }
+    
 
-        /*Mengecek apakah nama tersebut ada pada store*/
+    /*Mengecek apakah nama tersebut ada pada store*/
 
-        /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
+    /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
 
 
-        if (Mark_Supply != 0){
-            printf("Barang dengan nama yang sama sudah ada di toko!\n");
-        }
+    if (Mark_Supply != 0){
+        printf("Barang dengan nama yang sama sudah ada di toko!\n");
+    }
 
-        else if (Mark_Antrean == true){
-                printf("Barang dengan nama yang sama sudah ada di antrean!\n");
-        }  
+    else if (Mark_Antrean == true){
+            printf("Barang dengan nama yang sama sudah ada di antrean!\n");
+    }  
 
-        else{
-            enqueue(requestQueue, currentKata.buffer);
-            printf("Barang berhasil ditambahkan di antrean\n");
-        }
-
-        for (int z = 0; z < currentKata.length; z++){
-            currentKata.buffer[z] = '\0';
-        }
-        currentKata.length = 0;
-
-        printf("Masukkan nama item:\n");
-
-        startKataMajemuk(NULL);
+    else{
+        enqueue(requestQueue, currentKata.buffer);
+        printf("Barang berhasil ditambahkan di antrean\n");
     }
 }
