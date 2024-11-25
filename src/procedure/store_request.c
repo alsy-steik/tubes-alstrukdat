862b02b4c2d0b4db6496f005ef382a42bf0bc9c7
SArdyBramantyo <168545215+SArdyBramantyo@users.noreply.github.com>
2024-11-25 19:57:54 +0700

    Update store_request.c

--- src/procedure/store_request.c
+++ src/procedure/store_request.c
@@ -10,80 +10,67 @@
 /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
 
 
-void StoreRequest(ArrayDin arr, Queue requestQueue){
+void StoreRequest(ArrayDin arr, Queue* requestQueue){
     printf("Masukkan nama item:\n");
-
+    
     startKataMajemuk(NULL);
     /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
-    int Mark_Antrean = 0;
-    if (isEmpty(requestQueue)){
-        enqueue(&requestQueue, currentKata.buffer);
-    }
-
-    /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
-    
-    else{
-        Queue temp;
-        initQueue(&temp);
-        char* val;
-
-        
-        while (!isEmpty(requestQueue)){
-            val = dequeue(&requestQueue);
-            enqueue(&temp, val);
+    while (!is_same_string(currentKata.buffer, "Selesai")){
+        boolean Mark_Antrean;
+        printf("%d", isEmpty(*requestQueue));
+        if (isEmpty(*requestQueue)){
+            enqueue(requestQueue, currentKata.buffer);
         }
 
-        while (!isEmpty(temp)){
-            val = dequeue(&temp);
-            if (is_same_string(currentKata.buffer, val)){
-                Mark_Antrean++;
-            }
-            enqueue(&requestQueue, val);
-        }
-    }
+        /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
         
-    /*Mengecek apakah nama tersebut ada pada queue*/
+        else{
+            Mark_Antrean = contains(*requestQueue, currentKata.buffer);
+        }
+            
+        /*Mengecek apakah nama tersebut ada pada queue*/
 
-    /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
-    int Mark_Supply = 0;
-    int N = getArrayDinLength(arr);
+        /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
+        int Mark_Supply = 0;
+        int N = getArrayDinLength(arr);
 
-    if (N > 0){
-            for (int j = 0; j < N; j++){
-                Barang storelist = getArrayDinElmt(arr,j);
-                
-                if (is_same_string(currentKata.buffer, storelist.name)){
-                    Mark_Supply++;
+        if (N > 0){
+                for (int j = 0; j < N; j++){
+                    Barang storelist = getArrayDinElmt(arr,j);
+                    
+                    if (is_same_string(currentKata.buffer, storelist.name)){
+                        Mark_Supply++;
+                    }
                 }
-            }
-        /*Meskipun hanya memakai item_name, tetap perlu diambil harga_item untuk memisahkannya*/  
+            /*Meskipun hanya memakai item_name, tetap perlu diambil harga_item untuk memisahkannya*/  
+        }
+        
+
+        /*Mengecek apakah nama tersebut ada pada store*/
+
+        /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
+
+
+        if (Mark_Supply != 0){
+            printf("Barang dengan nama yang sama sudah ada di toko!\n");
+        }
+
+        else if (Mark_Antrean == true){
+                printf("Barang dengan nama yang sama sudah ada di antrean!\n");
+        }  
+
+        else{
+            enqueue(requestQueue, currentKata.buffer);
+            printf("Barang berhasil ditambahkan di antrean\n");
+        }
+
+        for (int z = 0; z < currentKata.length; z++){
+            currentKata.buffer[z] = '\0';
+        }
+        currentKata.length = 0;
+
+        printf("Masukkan nama item:\n");
+
+        startKataMajemuk(NULL);
     }
-    
-
-    /*Mengecek apakah nama tersebut ada pada store*/
-
-    /*----------------------------------------------------DIVIDER-----------------------------------------------------------*/
-
-
-    if (Mark_Supply != 0){
-        printf("Barang dengan nama yang sama sudah ada di toko!\n");
-    }
-
-    else if (Mark_Antrean != 0){
-        printf("Barang dengan nama yang sama sudah ada di antrean!\n");
-    }
-
-    else{
-        enqueue(&requestQueue, currentKata.buffer);
-        printf("Barang berhasil ditambahkan di antrean\n");
-    }
-
-    for (int z = 0; z < currentKata.length; z++){
-        currentKata.buffer[z] = '\0';
-    }
-    currentKata.length = 0;
-
-    printf("Masukkan nama item:\n");
-
-    startKataMajemuk(NULL);
 }
