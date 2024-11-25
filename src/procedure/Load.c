47146ae1bc0b6b926eb8af1aef4cec56fa278cdd
Dhafin <141107867+dhafin00@users.noreply.github.com>
2024-11-25 16:40:25 +0700

    Update Load.c

--- src/procedure/Load.c
+++ src/procedure/Load.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void Load(){
+  int fileAda = 0;
+
+  if (fileAda = 1) {
+    printf("Save file berhasil dibaca. PURRMART berhasil dijalankan.")
+  } else {
+    printf("Save file tidak ditemukan. PURRMART gagal dijalankan.")
+  }  
+
+  return;
+}
+
 
