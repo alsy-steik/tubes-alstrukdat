9983f5b7a34386224276415078ca7f40700497ad
SArdyBramantyo <168545215+SArdyBramantyo@users.noreply.github.com>
2024-12-22 01:04:57 +0700

    Update and rename wishlist_clear_new.c to wishlist_clear.c

---
+++ src/procedure/wishlist_clear.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+#include "doublyLinkedList.h"
+
+void Wishlist_clear(DoublyLinkedListNode **head){
+    *head = NULL;
+    /*Membuat linked list menjadi list baru yang empty*/
+}
+
+// int main() {
+//     DoublyLinkedListNode* wishlist = NULL;
+
+//     LinkedListInsertEnd(&wishlist, "Laptop");
+//     LinkedListInsertEnd(&wishlist, "Smartphone");
+//     LinkedListInsertEnd(&wishlist, "Headphone");
+//     LinkedListInsertEnd(&wishlist, "Monitor");
+//     LinkedListInsertEnd(&wishlist, "Keyboard");
+//     LinkedListInsertEnd(&wishlist, "Mouse");
+
+//     printf("\nCurrent wishlist:\n");
+//     printDoublyLinkedListNode(wishlist);
+
+//     Wishlist_clear(&wishlist);
+
+//     printDoublyLinkedListNode(wishlist);
+
+//     return 0;
+// }
+
+// gcc -o wishlist_clear wishlist_clear.c doublyLinkedList.c
