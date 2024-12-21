4799e8e2a55bf39e069b15bdfcf2d401b8c1335b
SArdyBramantyo <168545215+SArdyBramantyo@users.noreply.github.com>
2024-12-22 01:09:27 +0700

    Update wishlist_add.c

--- src/procedure/wishlist_add.c
+++ src/procedure/wishlist_add.c
@@ -69,7 +69,7 @@ void WishlistAdd(DoublyLinkedListNode** head, ArrayDin arr){
 // void store_list(ArrayDin arr)
 // {
 //     int N = getArrayDinLength(arr);
-//     printf("head barang yang ada di toko:\n");
+//     printf("List barang yang ada di toko:\n");
 //     if (N > 0)
 //     {
 //         for (int i = 0; i < N; i++)
