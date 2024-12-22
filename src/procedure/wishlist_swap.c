67c65a9695919af33618bf30778b2269545d620d
SArdyBramantyo <168545215+SArdyBramantyo@users.noreply.github.com>
2024-12-22 17:59:00 +0700

    Update wishlist_swap.c

--- src/procedure/wishlist_swap.c
+++ src/procedure/wishlist_swap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "boolean.h"
-#include "doublyLinkedList.h"
+#include "../boolean.h"
+#include "../adt/LinkedList/doublyLinkedList.h"
 
 // int NbElmt_list(DoublyLinkedListNode* head){
 //     int count = 0;
