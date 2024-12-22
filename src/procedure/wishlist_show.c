1968f075217b310966f301a76b6de604d36ce2cb
SArdyBramantyo <168545215+SArdyBramantyo@users.noreply.github.com>
2024-12-22 17:58:44 +0700

    Update wishlist_show.c

--- src/procedure/wishlist_show.c
+++ src/procedure/wishlist_show.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "boolean.h"
-#include "doublyLinkedList.h"
+#include "../boolean.h"
+#include "../adt/LinkedList/doublyLinkedList.h"
 
 void Wishlist_show(DoublyLinkedList list){
     printDoublyLinkedList(list);
